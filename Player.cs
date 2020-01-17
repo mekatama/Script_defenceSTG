7947b2fc22b1b3e4fc8a8bae2624dde0ad3f2c4a
mekatama <tamatte@hotmail.com>
2020-01-17 20:56:25 +0900

    壁に接触したら処理

---
+++ Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour{
+	public bool isWallHit_R;//wall hit flag
+	public bool isWallHit_L;//wall hit flag
+
+	void Start(){
+		isWallHit_R = false;//初期化
+		isWallHit_L = true;//初期化、右に移動したい
+	}
+
+	//他のオブジェクトとの当たり判定
+	void OnTriggerEnter( Collider other) {
+		if(other.tag == "Wall_R"){
+			isWallHit_R = true;
+			isWallHit_L = false;
+		}
+		if(other.tag == "Wall_L"){
+			isWallHit_R = false;
+			isWallHit_L = true;
+		}
+	}
+}
