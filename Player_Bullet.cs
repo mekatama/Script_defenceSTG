991f93f77ea6fb44917b9d08f08002aa5e97cc2d
mekatama <tamatte@hotmail.com>
2020-01-24 20:01:16 +0900

    06消去時に画面内の弾数減算

--- Player_Bullet.cs
+++ Player_Bullet.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Bullet : MonoBehaviour{
+	GameObject gameController;	//検索したオブジェクト入れる用
 	public float speed = 0.0f;	//移動speed
 
 	void Start(){
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerを探す
 	}
 
 	void Update(){
@@ -15,6 +17,10 @@ public class Player_Bullet : MonoBehaviour{
 	//他のオブジェクトとの当たり判定
 	void OnTriggerEnter( Collider other) {
 		if(other.tag == "Wall_Up"){
+			//gcって仮の変数にGameControllerのコンポーネントを入れる
+			GameController gc = gameController.GetComponent<GameController>();
+			//画面内の弾数を減算
+			gc.shotNum --;
 			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
 		}
 	}
