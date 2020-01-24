9fce4ccbe5e8d391359f0883ee07060ec9908a52
mekatama <tamatte@hotmail.com>
2020-01-24 20:01:44 +0900

    06発射時に画面内の弾数加算

--- Player_Shot.cs
+++ Player_Shot.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Shot : MonoBehaviour{
+	GameObject gameController;						//検索したオブジェクト入れる用
 	public GameObject bulletObject = null;			//弾プレハブ
 	public Transform bulletStartPosition = null;	//弾の発射位置を取得する
 
+	void Start(){
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerを探す
+	}
+
 	//bullet発射
 	public void PlayerShot(){
-		//弾を生成する位置を指定
-		Vector3 vecBulletPos = bulletStartPosition.position;
-		//弾を生成
-		Instantiate(bulletObject, vecBulletPos, transform.rotation);
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		if(gc.isShot){
+			//弾を生成する位置を指定
+			Vector3 vecBulletPos = bulletStartPosition.position;
+			//弾を生成
+			Instantiate(bulletObject, vecBulletPos, transform.rotation);
+			//画面内の弾数を加算
+			gc.shotNum ++;
+		}
 	}
 }
