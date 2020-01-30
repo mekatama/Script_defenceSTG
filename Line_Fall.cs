28d691a7f2a30fe85a6c53f3480d0e03988f6b37
mekatama <tamatte@hotmail.com>
2020-01-30 19:53:20 +0900

    防衛線の強制停止

---
+++ Line_Fall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line_Fall : MonoBehaviour{
+	private Rigidbody rb;	//入れる用
+//rigidbody.velocity = Vector3.zero;
+	void Start(){
+		rb = GetComponent<Rigidbody>();	//取得
+		rb.velocity = Vector3.zero;		//強制停止
+		rb.isKinematic = true;			//重力もキャンセル
+	}
+
+	void Update(){
+	}
+}
