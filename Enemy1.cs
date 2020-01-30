790722ad413a1286723d9da8c92dce7c12ef0fa6
mekatama <tamatte@hotmail.com>
2020-01-30 19:52:56 +0900

    弾と接触判定

---
+++ Enemy1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour{
+	//他のオブジェクトとの当たり判定(triger)
+	void OnTriggerEnter( Collider other) {
+		if(other.tag == "Bullet"){
+			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
+		}
+	}
+}
