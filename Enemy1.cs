e2ad5c49080858916feb158cfaa6524a236dbe92
mekatama <tamatte@hotmail.com>
2020-02-04 20:35:20 +0900

    ★12防衛線との接触判定

--- Enemy1.cs
+++ Enemy1.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy1 : MonoBehaviour{
+	GameObject gameController;	//検索したオブジェクト入れる用
+	private bool isTouch;		//接触flag
+	private bool isDead;		//死亡flag
+
+	void Start(){
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerを探す
+		isTouch = false;	//初期化
+		isDead = false;		//初期化
+	}
+
+	void Update(){
+		//lineから離れた判定
+		if(isTouch == true){
+			if(isDead == true){
+				//gcって仮の変数にGameControllerのコンポーネントを入れる
+				GameController gc = gameController.GetComponent<GameController>();
+				//画面内の弾数を減算
+				gc.lineEnemyNum --;
+				Debug.Log("linehanareru:" + gc.lineEnemyNum);
+				isTouch = false;
+			}
+		}
+	}
+
 	//他のオブジェクトとの当たり判定(triger)
 	void OnTriggerEnter( Collider other) {
 		if(other.tag == "Bullet"){
-			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
+			isDead = true;
+			Destroy(gameObject,0.1f);	//このGameObjectを［Hierrchy］ビューから削除する
+		}
+		if(other.tag == "Line"){
+			if(isTouch == false){
+				//gcって仮の変数にGameControllerのコンポーネントを入れる
+				GameController gc = gameController.GetComponent<GameController>();
+				//画面内の弾数を加算
+				gc.lineEnemyNum ++;
+				Debug.Log("lineTouch:" + gc.lineEnemyNum);
+				isTouch = true;
+			}
 		}
 	}
 }
--- GameController.cs
+++ GameController.cs
@@ -6,14 +6,16 @@ public class GameController : MonoBehaviour{
 	public int shotNum;		//画面内の弾数
 	public int shotNumMax;	//画面内の弾数MAX
 	public bool isShot;		//発射flag
+	public int lineEnemyNum;//防衛線に乗っている敵の数
+
 
 	void Start(){
-		shotNum = 0;	//初期化
-		isShot = false;	//初期化
+		shotNum = 0;		//初期化
+		isShot = false;		//初期化
+		lineEnemyNum = 0;	//初期化
 	}
 
 	void Update(){
-		Debug.Log("shot:" + isShot);
 		//画面内の弾数で攻撃可能か判定
 		if(shotNum >= shotNumMax){
 			isShot = false;
