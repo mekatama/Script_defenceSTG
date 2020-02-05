86b983efc759b15a3a378cd5d080c37ba699dfdd
mekatama <tamatte@hotmail.com>
2020-02-05 21:01:41 +0900

    13落下処理

--- Line_Fall.cs
+++ Line_Fall.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Line_Fall : MonoBehaviour{
-	private Rigidbody rb;	//入れる用
+	GameObject gameController;	//検索したオブジェクト入れる用
+	private Rigidbody rb;		//入れる用
 
 	void Start(){
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerを探す
 		rb = GetComponent<Rigidbody>();	//取得
 		rb.velocity = Vector3.zero;		//強制停止
 		rb.isKinematic = true;			//重力もキャンセル
 	}
 
 	void Update(){
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		if(gc.isFall == true){
+			rb.AddForce(0, 0, 0);		//動けるようにする
+			rb.isKinematic = false;		//重力を有効にする
+		}
 	}
 }
