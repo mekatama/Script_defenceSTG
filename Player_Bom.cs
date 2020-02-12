02a52acb9755e59f6902c589a27bcf59e9f080fd
mekatama <tamatte@hotmail.com>
2020-02-12 19:57:11 +0900

    17Bom用スクリプト

---
+++ Player_Bom.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Bom : MonoBehaviour{
+	GameObject gameController;	//検索したオブジェクト入れる用
+	public float speed = 0.0f;	//移動speed
+
+	void Start(){
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerを探す
+	}
+
+	void Update(){
+		//移動
+			transform.position += transform.up * speed * Time.deltaTime;
+	}
+	//他のオブジェクトとの当たり判定(triger)
+	void OnTriggerEnter( Collider other) {
+		if(other.tag == "Wall_Up"){
+			//gcって仮の変数にGameControllerのコンポーネントを入れる
+			GameController gc = gameController.GetComponent<GameController>();
+			//画面内の弾数を減算
+			gc.shotNum --;
+			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
+		}
+		if(other.tag == "Enemy"){
+			//gcって仮の変数にGameControllerのコンポーネントを入れる
+//			GameController gc = gameController.GetComponent<GameController>();
+			//画面内の弾数を減算
+//			gc.shotNum --;
+//			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
+		}
+	}
+}
