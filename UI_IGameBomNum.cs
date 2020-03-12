e609552ba1a1ae844861359c78125d4520ee55ad
mekatama <tamatte@hotmail.com>
2020-03-12 19:00:58 +0900

    28InGaame用スコアとボムUI

---
+++ UI_IGameBomNum.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_IGameBomNum : MonoBehaviour{
+	public GameObject gameController;	//GameController取得
+	public Text bomNumText;				//Textコンポーネント取得用
+
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//Bom数表示
+		bomNumText.text = "BOM : " + gc.bomNum.ToString("000000");
+	}
+}
