097e1a03ff18876df7c05cc33802ab07445a8096
mekatama <tamatte@hotmail.com>
2020-03-23 22:06:08 +0900

    29タイトル画面内の制御

---
+++ TitleController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour{
+
+	void Update(){
+		//backキー
+		if (Input.GetKeyUp(KeyCode.Escape)){
+			Application.Quit();	//アプリ終了
+		}
+	}
+
+	//Start用の制御関数
+	public void ButtonClicked_Start(){
+		SceneManager.LoadScene("main1");	//シーンのロード
+	}
+
+	//SetUp用の制御関数
+	public void ButtonClicked_SetUp(){
+		SceneManager.LoadScene("SetUp");	//シーンのロード
+	}
+
+	//HowTo用の制御関数
+	public void ButtonClicked_HowToPlay(){
+		SceneManager.LoadScene("HowToPlay");	//シーンのロード
+	}
+
+	//アプリ終了
+	public void ButtonClicked_Exit(){
+		Application.Quit();
+		Debug.Log("exit");	
+	}
+
+	//Debug用ハイスコアリセットボタン
+//	public void ButtonClicked_Reset(){
+//		PlayerPrefs.DeleteAll();
+//		Debug.Log("全データ削除しますた");	
+//	}
+}
