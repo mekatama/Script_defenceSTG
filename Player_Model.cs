fbad787351186a06bbcae2367f6a725ad9ff057a
mekatama <tamatte@hotmail.com>
2020-04-03 16:52:17 +0900

    31saveの値を見てモデルを切り替える

--- Player_Model.cs
+++ Player_Model.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Model : MonoBehaviour{
+//	GameObject gameController;			//検索したオブジェクト入れる用
 	public GameObject playerModel01;	//自機model
 	public GameObject playerModel02;	//自機model
 	public GameObject playerModel03;	//自機model
-	private int playerVer;				//自機model制御用
+	private int playerBody;				//自機model制御用
 
 	void Start(){
-		playerVer = 2;
-		switch(playerVer){
+		//SetUpデータをLosdする 値がなかったら０を入れて初期化
+		playerBody = PlayerPrefs.GetInt("PlayerBody", 0); 
+		Debug.Log("body : " + playerBody);
+//		gameController = GameObject.FindWithTag ("GameController");	//GameControllerを探す
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+//		GameController gc = gameController.GetComponent<GameController>();
+		Debug.Log("body : " + playerBody);
+//		gc.playerBody = 2;
+		switch(playerBody){
 			case 1:
 				Destroy(playerModel02);
 				Destroy(playerModel03);
