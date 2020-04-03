7c43827c2dbfa911229801dc22fe1cf1a4faf890
mekatama <tamatte@hotmail.com>
2020-04-03 16:52:55 +0900

    31SetUp画面の仮処理

--- SetUpController.cs
+++ SetUpController.cs
@@ -4,6 +4,30 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SetUpController : MonoBehaviour{
+	private int playerBody;		//自機model制御用
+
+	void Start(){
+		//SetUpデータをSaveする 値がなかったら０を入れて初期化
+//		playerBody = 2;
+		PlayerPrefs.SetInt("PlayerBody", playerBody);		//save
+	}
+
+	//1用の制御関数
+	public void ButtonClicked_1(){
+		playerBody = 1;
+		PlayerPrefs.SetInt("PlayerBody", playerBody);		//save
+	}
+	//2用の制御関数
+	public void ButtonClicked_2(){
+		playerBody = 2;
+		PlayerPrefs.SetInt("PlayerBody", playerBody);		//save
+	}
+	//3用の制御関数
+	public void ButtonClicked_3(){
+		playerBody = 3;
+		PlayerPrefs.SetInt("PlayerBody", playerBody);		//save
+	}
+
 	//return用の制御関数
 	public void ButtonClicked_Return(){
 		SceneManager.LoadScene("title");	//シーンのロード
